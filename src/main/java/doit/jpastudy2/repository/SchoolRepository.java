849ac22f1868630e92a9930b64443033b9655312
sebeeeen <a856412@gmail.com>
2024-10-02 10:50:04 +0900

    git test

--- src/main/java/doit/jpastudy2/repository/SchoolRepository.java
+++ src/main/java/doit/jpastudy2/repository/SchoolRepository.java
@@ -9,7 +9,6 @@ public interface SchoolRepository extends JpaRepository<School, Long> {
     // ( ) = 조건
     // find + [ ] + By + (조건)
 
-    // select * from Category
     School findBySchoolName(String schoolName);
     // select * from Category where type = ? and description = ?
     School findBySchoolNameAndTotalStudents(String schoolName, int totalStudents);
